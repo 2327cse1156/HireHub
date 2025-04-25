f91d299d91064fcab7e0aadc62a55c4d8f88f85d
2327cse1156 <anshkaushal92@gmail.com>
2025-04-26 01:18:32 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
+import "./config/instrument.js"
 import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
+import * as Sentry from "@sentry/node"
 import connectDB from './config/db.js';
+import { clerkWebhooks } from "./controllers/webhooks.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+Sentry.setupExpressErrorHandler(app);
 
+await connectDB();
 // Middleware
 app.use(cors());
 app.use(json());
@@ -14,7 +19,12 @@ app.use(json());
 app.get('/', (req, res) => {
   res.send('Welcome to the Job Portal API');
 });
-await connectDB();
+app.get("/debug-sentry",function mainHandler(req,res){
+  throw new Error("My first Sentry error!");
+})
+app.post("/webhooks",clerkWebhooks)
+
+
 
 // Start the server
 app.listen(PORT, () => {
